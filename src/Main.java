80fe75b2b786d11324d4788023402ca5585a250f
LEEGYEONGHA <haha-3208@khu.ac.kr>
2025-07-02 22:43:42 +0900

    2.2-1) read()로 입력받기

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Java");
+    public static void main(String[] args) throws IOException {
+        System.out.println((char)System.in.read());
     }
 }
