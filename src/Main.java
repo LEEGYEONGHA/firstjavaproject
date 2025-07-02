4a1c8dc99ded05afca878a9f11d5337a8df1bc16
LEEGYEONGHA <haha-3208@khu.ac.kr>
2025-07-03 01:31:35 +0900

    2.2-2) Scanner 클래스로 입력받기

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println((char)System.in.read());
+        System.out.print((char)System.in.read());
+        System.out.print((char)System.in.read());
+        System.out.print((char)System.in.read());
+        // 123을 입력하게 되면, 순서대로 맨 첫 줄은 1, 그 다음 줄은 2, 맨 마지막 줄은 3을 읽어오는 건가 봄.
+        // read() 명령어가 값을 1바이트씩만 읽어온다는 건 알았는데, 111 느낌으로 출력되는 줄 알았음...
     }
 }
