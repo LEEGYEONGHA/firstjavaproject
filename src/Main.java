a5686881cf8d6fdd65781e4663966694f2790082
LEEGYEONGHA <haha-3208@khu.ac.kr>
2025-07-03 19:57:03 +0900

    3.2.3) printf()로 서식 지정해서 출력하기

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println(scan.nextLine());
-        System.out.println(scan.nextLine());
-        System.out.println(scan.nextLine());
-        scan.close();
+        System.out.printf("이름: %s%n나이: %d\n취미: %s", "코천이", 21, "코딩");
     }
 }
