74754ded69196b9d295a92917eb181ddb3235733
LEEGYEONGHA <haha-3208@khu.ac.kr>
2025-07-05 23:35:18 +0900

    25.07.05-2

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("이름: %s%n나이: %d\n취미: %s", "코천이", 21, "코딩");
+        int score = 90;
+        if (score >= 90) {
+            System.out.println("A학점입니다.");
+        }
     }
 }
