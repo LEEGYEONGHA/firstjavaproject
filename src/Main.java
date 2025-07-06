8e9347f2b4504cf19543d6c76b58ff45da851579
LEEGYEONGHA <haha-3208@khu.ac.kr>
2025-07-06 13:40:05 +0900

    5.1.2) 증감 연산자

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int score = 90;
-        if (score >= 90) {
-            System.out.println("A학점입니다.");
-        }
+        int num1 = 5;
+        int num2 = 5;
+        int result1 = num1++;
+        int result2 = ++num2;
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(num1);
+        System.out.println(num2);
     }
 }
