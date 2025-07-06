7ac18a556c34bc080dea6433ae685c833bde2ecc
LEEGYEONGHA <haha-3208@khu.ac.kr>
2025-07-06 17:01:37 +0900

    6.2.2) 이차원 배열에 접근하기

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
 // import java.util.Scanner;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    int i = 2;
-    while (i <= 9) {
-        System.out.println("---" + i + "단 ---");
-        i ++;
-        int j = 1;
-        while (j <= 9) {
-            System.out.println(i + "x " + j + "=" + (i * j));
-            j++;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j]+ " "); // 값 구분을 위해 공백 추가
+            }
+            System.out.println(""); // 행 구분을 위해 줄 바꿈 추가
         }
-    }
-    }
 }
