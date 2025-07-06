ba5ec615b2ae430e364a49c782960c248e63116a
LEEGYEONGHA <haha-3208@khu.ac.kr>
2025-07-06 13:52:46 +0900

    5.2) 1분퀴즈 3. 답

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 5;
-        int result1 = num1++;
-        int result2 = ++num2;
-        System.out.println(result1);
-        System.out.println(result2);
-        System.out.println(num1);
-        System.out.println(num2);
+    int i = 2;
+    while (i <= 9) {
+        System.out.println("---" + i + "단 ---");
+        i ++;
+        int j = 1;
+        while (j <= 9) {
+            System.out.println(i + "x " + j + "=" + (i * j));
+            j++;
+        }
+    }
     }
 }
