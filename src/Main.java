c0489e205bc5af68a42e2d7d7b317d0f1f9bc692
LEEGYEONGHA <haha-3208@khu.ac.kr>
2025-07-07 22:42:01 +0900

    6.3.2) 1분퀴즈 3. 답

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-// import java.util.Scanner;
 
-import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j]+ " "); // 값 구분을 위해 공백 추가
-            }
-            System.out.println(""); // 행 구분을 위해 줄 바꿈 추가
+        int[] original = {1, 2, 3, 4, 5};
+        int[] copy = new int[original.length];
+        for (int i = 0; i < original.length; i++) {
+            copy[i] = original[i];
         }
+        copy[3] = 99;
+        System.out.println(original[3]);
+    }
 }
